930f1678b6a3f08bd8ae5af5c023c297bce05210
Joe Takizawa <Joe.Takizawa@mecto.jp>
2026-02-14 23:37:34 +0900

    修正

--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,7 @@ import sys
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
